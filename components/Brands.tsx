95fbf544dbdfa164c62ef4306543ad8641b22619
developerYeasin <yea13sin@gmail.com>
2022-04-25 19:49:56 +0600

    laskdjflk

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -3,43 +3,43 @@ import { BiWorld } from "react-icons/bi";
 const Brands = () => {
   return (
     <div className="brands">
-      <h3>Lorem</h3>
+      <h3>카테고리</h3>
       <div className="brand-items">
         <div className="item">
           <span>
-            <BiWorld />
+            <img src="img/drawable/icon_01.svg" alt="" />
           </span>
-          <p>dummy</p>
+          <p>실버 여행</p>
         </div>
         <div className="item">
           <span>
-            <BiWorld />
+            <img src="img/drawable/icon_02.svg" alt="" />
           </span>
-          <p>dummy</p>
+          <p>건강 BIO</p>
         </div>
         <div className="item">
           <span>
-            <BiWorld />
+            <img src="img/drawable/icon_03.svg" alt="" />
           </span>
-          <p>dummy</p>
+          <p>실버정보</p>
         </div>
         <div className="item">
           <span>
-            <BiWorld />
+            <img src="img/drawable/icon_04.svg" alt="" />
           </span>
-          <p>dummy</p>
+          <p>실버 시설</p>
         </div>
         <div className="item">
           <span>
-            <BiWorld />
+            <img src="img/drawable/icon_05.svg" alt="" />
           </span>
-          <p>dummy</p>
+          <p>아름다운 준비</p>
         </div>
         <div className="item">
           <span>
-            <BiWorld />
+            <img src="img/drawable/icon_06.svg" alt="" />
           </span>
-          <p>dummy</p>
+          <p>마이페이지</p>
         </div>
       </div>
     </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
 const Header = () => {
   return (
-    <div className="header">
+    <div
+      className="header"
+      style={{ backgroundImage: "url(img/drawable/bg_img.svg)" }}
+    >
       <div className="container">
         <div className="left-part">
-          <h3>Lorem Ipsum is simply dummy</h3>
-          <button>Get Started</button>
+          <h3>나를 위한 <span>건강정보를</span> 지금 만나보세요</h3>
+          <button>자세히 보기</button>
         </div>
         <div className="right-part"></div>
       </div>
